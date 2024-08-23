c87a2cd3c320bd70229d9bd73ab7c2a3d8ec2de8
ritika sharma <ritika.shamra@axeno.co>
2024-08-23 14:55:50 +0530

    filtering tasks by their priority and status

--- build/index.js
+++ build/index.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var add = document.getElementsByClassName("add-btn")[0];
 var cardContainer = document.getElementsByClassName("card-container")[0];
 var title = document.getElementsByClassName("title-inp")[0];
@@ -7,20 +18,10 @@ var category = document.getElementsByClassName("category-val")[0];
 var categoryDiv = document.getElementsByClassName("category-inp")[0];
 var priorityVal = document.getElementsByClassName("priority-inp")[0];
 var dateVal = document.getElementsByClassName("date-inp")[0];
-var searchVal = document.getElementsByClassName('search')[0];
-category.addEventListener("change", function () {
-    var crossBtn = document.createElement("button");
-    crossBtn.classList.add("cross-btn");
-    crossBtn.innerHTML = "<i class=\"fa-solid fa-xmark\"></i>";
-    var val = document.createElement("span");
-    val.classList.add("category-value");
-    val.textContent = category.value;
-    val.appendChild(crossBtn);
-    categoryDiv.appendChild(val);
-    crossBtn.addEventListener("click", function () {
-        categoryDiv.removeChild(val);
-    });
-});
+var searchVal = document.getElementsByClassName("search")[0];
+var priorityValRight = document.getElementsByClassName("priority-right")[0];
+var iscompleted = document.getElementsByClassName("filter-inp")[0];
+console.log("hello ritika");
 var tasks = [];
 add.addEventListener("click", function (e) {
     e.preventDefault();
@@ -34,26 +35,81 @@ add.addEventListener("click", function (e) {
 function generateArray(tasks) {
     var titleName = title.value;
     var descName = desc.value;
-    tasks.push({ titleName: titleName, descName: descName });
+    var priorityName = priorityVal.value;
+    var completed = false;
+    tasks.push(__assign(__assign({}, tasks), { titleName: titleName, descName: descName, priorityName: priorityName, completed: completed }));
     console.log(tasks);
     createCards(tasks);
 }
-console.log("ttt", tasks.length);
-searchVal.addEventListener('input', function () { return filterSearch(tasks); });
+category.addEventListener("change", function () {
+    var crossBtn = document.createElement("button");
+    crossBtn.classList.add("cross-btn");
+    crossBtn.innerHTML = "<i class=\"fa-solid fa-xmark\"></i>";
+    var val = document.createElement("span");
+    val.classList.add("category-value");
+    val.textContent = category.value;
+    val.appendChild(crossBtn);
+    categoryDiv.appendChild(val);
+    crossBtn.addEventListener("click", function () {
+        categoryDiv.removeChild(val);
+    });
+});
+searchVal.addEventListener("input", function () { return filterSearch(tasks); });
 function filterSearch(tasks) {
     var val = searchVal.value.trim();
+    // const taskComplete = iscompleted.value === "complete";
     if (val === "")
         createCards(tasks);
     var filterTasks = tasks.filter(function (task) {
-        return task.titleName.toLowerCase().includes(val.toLowerCase());
+        return ((task.titleName.toLowerCase().includes(val.toLowerCase()) || task.descName.toLowerCase().includes(val.toLowerCase())));
     });
     console.log("ritika", filterTasks);
     createCards(filterTasks);
 }
+priorityValRight.addEventListener("change", function () { return filterPriority(tasks); });
+function filterPriority(tasks) {
+    var order = priorityValRight.value;
+    var filterarr = sortPriority(tasks, order);
+    if (order === "")
+        createCards(tasks);
+    else
+        createCards(filterarr);
+}
+function sortPriority(tasks, order) {
+    var priorityMap = {
+        low: 1,
+        medium: 2,
+        high: 3,
+    };
+    return tasks.sort(function (a, b) {
+        var p1 = priorityMap[a.priorityName];
+        var p2 = priorityMap[b.priorityName];
+        if (order === "asc") {
+            return p1 - p2;
+        }
+        else {
+            return p2 - p1;
+        }
+    });
+}
+iscompleted.addEventListener("change", function () { return filterCategory(tasks); });
+function filterCategory(tasks) {
+    if (iscompleted.value === "all") {
+        createCards(tasks);
+        return;
+    }
+    var priority = priorityValRight.value;
+    var val = iscompleted.value === "complete";
+    var filterTasks = tasks.filter(function (task) {
+        return (task.completed === val);
+    });
+    console.log("Filtered Tasks", filterTasks);
+    createCards(filterTasks);
+}
 function createCards(tasks) {
+    cardContainer.innerHTML = "";
     if (tasks.length === 0)
-        generateArray(tasks);
-    cardContainer.innerHTML = '';
+        return;
     tasks.forEach(function (task) {
         var card = document.createElement("div");
         card.classList.add("task-card");
@@ -61,6 +117,10 @@ function createCards(tasks) {
         leftDiv.classList.add("leftDiv");
         var check = document.createElement("input");
         check.setAttribute("type", "checkbox");
+        check.checked = task.completed;
+        check.addEventListener("change", function () {
+            task.completed = check.checked;
+        });
         leftDiv.appendChild(check);
         card.appendChild(leftDiv);
         var rightDiv = document.createElement("div");
@@ -75,7 +135,7 @@ function createCards(tasks) {
         priority.textContent =
             dateVal.value +
                 "   |   12:10 in the afternoon   |   " +
-                priorityVal.value +
+                task.priorityName +
                 " priority";
         priorityDiv.appendChild(priority);
         rightDiv.appendChild(priorityDiv);
@@ -89,4 +149,5 @@ function createCards(tasks) {
     });
     title.value = "";
     desc.value = "";
+    priorityVal.value = "";
 }
--- src/index.ts
+++ src/index.ts
@@ -20,8 +20,50 @@ const priorityVal = document.getElementsByClassName(
 const dateVal = document.getElementsByClassName(
   "date-inp"
 )[0] as HTMLInputElement;
-const searchVal = document.getElementsByClassName('search')[0] as HTMLInputElement;
+const searchVal = document.getElementsByClassName(
+  "search"
+)[0] as HTMLInputElement;
+const priorityValRight = document.getElementsByClassName(
+  "priority-right"
+)[0] as HTMLInputElement;
+const iscompleted = document.getElementsByClassName(
+  "filter-inp"
+)[0] as HTMLInputElement;
+console.log("hello ritika");
 
+type Priority = "low" | "medium" | "high";
+
+type Tasks = {
+  titleName: string;
+  descName: string;
+  priorityName: Priority;
+  completed: boolean;
+};
+
+const tasks: Tasks[] = [];
+
+add.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (title.value === "") return;
+  else if (desc.value === "") return;
+  else generateArray(tasks);
+});
+
+function generateArray(tasks: Tasks[]): void {
+  const titleName = title.value;
+  const descName = desc.value;
+  const priorityName = priorityVal.value as Priority;
+  const completed = false;
+  tasks.push({
+    ...tasks,
+    titleName: titleName,
+    descName: descName,
+    priorityName: priorityName,
+    completed: completed,
+  });
+  console.log(tasks);
+  createCards(tasks);
+}
 
 category.addEventListener("change", () => {
   const crossBtn = document.createElement("button");
@@ -37,75 +79,105 @@ category.addEventListener("change", () => {
   });
 });
 
-type Tasks = {
-  titleName: string;
-  descName: string;
-}
+searchVal.addEventListener("input", () => filterSearch(tasks));
 
-const tasks: Tasks[]=[];
-
-add.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (title.value === "") return;
-  else if (desc.value === "") return;
-  else generateArray(tasks);
-});
-
-function generateArray(tasks:Tasks[]):void{
-  const titleName = title.value;
-  const descName = desc.value;
-  tasks.push({titleName:titleName,descName:descName});
-  console.log(tasks);
-  createCards(tasks);
-}
-console.log("ttt",tasks.length)
-
-  searchVal.addEventListener('input',()=>filterSearch(tasks));
-function filterSearch(tasks:Tasks[]):void{
+function filterSearch(tasks: Tasks[]): void {
   const val = searchVal.value.trim();
-  if(val==="") createCards(tasks);
-  const filterTasks = tasks.filter((task)=>{return task.titleName.toLowerCase().includes(val.toLowerCase());
+  // const taskComplete = iscompleted.value === "complete";
+  if (val === "") createCards(tasks);
+  const filterTasks = tasks.filter((task) => {
+    return (
+      (task.titleName.toLowerCase().includes(val.toLowerCase()) || task.descName.toLowerCase().includes(val.toLowerCase())) 
+    );
   });
-  console.log("ritika",filterTasks);
+  console.log("ritika", filterTasks);
   createCards(filterTasks);
 }
 
-function createCards(tasks:Tasks[]): void {
-  if(tasks.length === 0) generateArray(tasks);
-  cardContainer.innerHTML = '';
-  tasks.forEach((task)=>{
+priorityValRight.addEventListener("change", () => filterPriority(tasks));
+
+function filterPriority(tasks: Tasks[]): void {
+  const order = priorityValRight.value;
+  const filterarr = sortPriority(tasks,order);
+  if(order === "") createCards(tasks);
+  else createCards(filterarr);
+}
+
+function sortPriority(tasks:Tasks[],order:String):Tasks[]{
+  const priorityMap : Record<Priority,number> = {
+    low:1,
+    medium:2,
+    high:3,
+  };
+  return tasks.sort((a,b)=>{
+    const p1 = priorityMap[a.priorityName as Priority];
+    const p2 = priorityMap[b.priorityName as Priority];
+
+    if(order === "asc"){
+      return p1-p2;
+    } else{
+      return p2-p1;
+    }
+  });
+}
+
+iscompleted.addEventListener("change", () => filterCategory(tasks));
+
+function filterCategory(tasks: Tasks[]): void {
+  if (iscompleted.value === "all") {
+    createCards(tasks);
+    return;
+  }
+  const priority = priorityValRight.value;
+  const val = iscompleted.value === "complete";
+  const filterTasks = tasks.filter((task) => {
+    return (task.completed === val)
+  });
+  console.log("Filtered Tasks", filterTasks);
+  createCards(filterTasks);
+}
+
+function createCards(tasks: Tasks[]): void {
+  cardContainer.innerHTML = "";
+  if (tasks.length === 0) return;
+  tasks.forEach((task) => {
     const card = document.createElement("div");
-  card.classList.add("task-card");
-  const leftDiv = document.createElement("div");
-  leftDiv.classList.add("leftDiv");
-  const check = document.createElement("input");
-  check.setAttribute("type", "checkbox");
-  leftDiv.appendChild(check);
-  card.appendChild(leftDiv);
-  const rightDiv = document.createElement("div");
-  rightDiv.classList.add("rightDiv");
-  const cardTitle = document.createElement("h4");
-  cardTitle.classList.add("title");
-  cardTitle.textContent = task.titleName;
-  rightDiv.appendChild(cardTitle);
-  rightDiv.appendChild(categoryDiv);
-  const priorityDiv = document.createElement("div");
-  const priority = document.createElement("span");
-  priority.textContent =
-    dateVal.value +
-    "   |   12:10 in the afternoon   |   " +
-    priorityVal.value +
-    " priority";
-  priorityDiv.appendChild(priority);
-  rightDiv.appendChild(priorityDiv);
-  // rightDiv.appendChild(categoryDiv);
-  const cardDesc = document.createElement("p");
-  cardDesc.classList.add("desc");
-  cardDesc.textContent = task.descName;
-  rightDiv.appendChild(cardDesc);
-  card.appendChild(rightDiv);
-  cardContainer.appendChild(card);
-  })
+    card.classList.add("task-card");
+    const leftDiv = document.createElement("div");
+    leftDiv.classList.add("leftDiv");
+    const check = document.createElement("input");
+    check.setAttribute("type", "checkbox");
+    check.checked = task.completed;
+    check.addEventListener("change", () => {
+      task.completed = check.checked;
+    });
+    leftDiv.appendChild(check);
+    card.appendChild(leftDiv);
+    const rightDiv = document.createElement("div");
+    rightDiv.classList.add("rightDiv");
+    const cardTitle = document.createElement("h4");
+    cardTitle.classList.add("title");
+    cardTitle.textContent = task.titleName;
+    rightDiv.appendChild(cardTitle);
+    rightDiv.appendChild(categoryDiv);
+    const priorityDiv = document.createElement("div");
+    const priority = document.createElement("span");
+    priority.textContent =
+      dateVal.value +
+      "   |   12:10 in the afternoon   |   " +
+      task.priorityName +
+      " priority";
+    priorityDiv.appendChild(priority);
+    rightDiv.appendChild(priorityDiv);
+    // rightDiv.appendChild(categoryDiv);
+    const cardDesc = document.createElement("p");
+    cardDesc.classList.add("desc");
+    cardDesc.textContent = task.descName;
+    rightDiv.appendChild(cardDesc);
+    card.appendChild(rightDiv);
+    cardContainer.appendChild(card);
+  });
   title.value = "";
   desc.value = "";
+  priorityVal.value = "";
 }
